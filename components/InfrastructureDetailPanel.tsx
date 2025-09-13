26c62db188a945f3c020d3c7a8fa9eafbb4de940
aryaw05 <aryawahyuaw77@gmail.com>
2025-09-13 19:10:26 +0700

    feat(InfrastructureDetailPanel): update status display and task visualization

--- components/InfrastructureDetailPanel.tsx
+++ components/InfrastructureDetailPanel.tsx
@@ -33,17 +33,28 @@ export default function InfrastructureDetailPanel({
 }: InfrastructureDetailPanelProps) {
   const getStatusBadge = (status: string) => {
     switch (status) {
-      case "normal":
-        return <Badge className="status-normal">Normal</Badge>;
-      case "progress":
-        return <Badge className="status-progress">On Progress</Badge>;
-      case "trouble":
-        return <Badge className="status-trouble">Trouble</Badge>;
+      case "CLOSED":
+        return <Badge className="bg-green-600">Closed</Badge>;
+      case "ON_PROGRESS":
+        return <Badge className="bg-yellow-400">On Progress</Badge>;
+      case "OPEN":
+        return <Badge className="bg-red-400">Open</Badge>;
       default:
         return <Badge variant="secondary">{status}</Badge>;
     }
   };
-
+  const getStatusIcon = (status: string) => {
+    switch (status) {
+      case "CLOSED":
+        return <CheckCircle className="w-4 h-4  text-green-400" />;
+      case "ON_PROGRESS":
+        return <Activity className="w-4 h-4 text-yellow-400" />;
+      case "OPEN":
+        return <AlertTriangle className="w-4 h-4 text-red-400" />;
+      default:
+        return <Clock className="w-4 h-4" />;
+    }
+  };
   const formatDate = (date: Date) => {
     return new Intl.DateTimeFormat("id-ID", {
       year: "numeric",
@@ -67,7 +78,7 @@ export default function InfrastructureDetailPanel({
       />
 
       {/* Panel content */}
-      <Card className="fixed bottom-0 left-0 right-0 max-h-[70vh] lg:static lg:max-h-none lg:w-96 lg:h-full shadow-elevation border-t lg:border lg:rounded-lg bg-card/95 backdrop-blur-sm">
+      <Card className="fixed bottom-0 left-0 right-0 max-h-[70vh] lg:static lg:max-h-none lg:w-96 lg:min-h-screen shadow-elevation border-t lg:border lg:rounded-lg bg-card/95 backdrop-blur-sm ">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-4">
           <CardTitle className="text-lg font-semibold">
             Infrastructure Detail
@@ -85,54 +96,15 @@ export default function InfrastructureDetailPanel({
                 <h3 className="text-xl font-bold">
                   {infrastructureDetail?.infrastructure_name}
                 </h3>
-                <p className="text-sm text-muted-foreground">
-                  ID: {infrastructureDetail?.id}
-                </p>
+
                 <p className="text-sm text-muted-foreground">
                   UPT: {infrastructureDetail?.upt.upt_name}
                 </p>
               </div>
-
-              {/* looping task */}
-              <div className="flex items-center justify-between">
-                <span className="text-xs text-muted-foreground">
-                  {/* Updated{" "}
-                  {formatDate(
-                    infrastructureDetail?.infrastructure_task[0]
-                      .created_at as Date
-                  )} */}
-                </span>
-                {infrastructureDetail &&
-                infrastructureDetail.infrastructure_task.length > 0 ? (
-                  <div className="space-y-2">
-                    {infrastructureDetail?.infrastructure_task.map((task) => (
-                      <div key={task.id} className="p-2 border rounded-md">
-                        <div className="flex justify-between items-center">
-                          <span className="text-xs text-muted-foreground">
-                            Updated {formatDate(new Date(task.created_at))}
-                          </span>
-                          {getStatusBadge(task.status)}
-                        </div>
-                        <p className="text-xs text-muted-foreground mt-1">
-                          {task.notes}
-                        </p>
-                        <p className="text-xs text-muted-foreground flex items-center gap-1 mt-1">
-                          <User className="w-3 h-3" />
-                          {task.operator}
-                        </p>
-                      </div>
-                    ))}
-                  </div>
-                ) : (
-                  <span className="text-xs text-muted-foreground">No Task</span>
-                )}
-              </div>
             </div>
-
             <Separator />
-
             {/* Infrastructure Details */}
-            {/* <div className="space-y-4">
+            <div className="space-y-4">
               <h4 className="font-semibold flex items-center gap-2">
                 <Building className="w-4 h-4" />
                 Infrastructure Info
@@ -141,47 +113,99 @@ export default function InfrastructureDetailPanel({
               <div className="grid gap-3 text-sm">
                 <div className="flex justify-between">
                   <span className="text-muted-foreground">Type:</span>
-                  <Badge variant="outline">{anomaly.type}</Badge>
+                  <Badge variant="outline">
+                    {
+                      infrastructureDetail?.infrastructure_type
+                        .infrastructure_type
+                    }
+                  </Badge>
                 </div>
-                <div className="flex justify-between">
-                  <span className="text-muted-foreground">UPT:</span>
-                  <span className="font-medium">{anomaly.upt}</span>
-                </div>
+                <div className="flex justify-between"></div>
                 <div className="flex justify-between items-start">
                   <span className="text-muted-foreground">Location:</span>
                   <div className="text-right">
                     <div className="flex items-center gap-1">
                       <MapPin className="w-3 h-3" />
                       <span className="font-mono text-xs">
-                        {anomaly.location.lat.toFixed(4)},{" "}
-                        {anomaly.location.lng.toFixed(4)}
+                        {infrastructureDetail?.latitude.toFixed(4)},
+                        {infrastructureDetail?.longitude.toFixed(4)}
                       </span>
                     </div>
                   </div>
                 </div>
               </div>
-            </div> */}
-
+            </div>
+            {/* {infrastructureDetail &&
+            infrastructureDetail.infrastructure_task.length > 0 ? (
+              <div className="space-y-2">
+                {infrastructureDetail?.infrastructure_task.map((task) => (
+                  <div key={task.id} className="p-2 border rounded-md">
+                    <div className="flex justify-between items-center">
+                      <span className="text-xs text-muted-foreground">
+                        Updated {formatDate(new Date(task.created_at))}
+                      </span>
+                      {getStatusBadge(task.status)}
+                    </div>
+                    <p className="text-xs text-muted-foreground mt-1">
+                      {task.notes}
+                    </p>
+                    <p className="text-xs text-muted-foreground flex items-center gap-1 mt-1">
+                      <User className="w-3 h-3" />
+                      {task.operator}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <span className="text-xs text-muted-foreground">No Task</span>
+            )} */}
             <Separator />
-
-            {/* Anomaly Description */}
-            {/* <div className="space-y-3">
+            <div className="space-y-4">
               <h4 className="font-semibold flex items-center gap-2">
-                <FileText className="w-4 h-4" />
-                Description
+                <Clock className="w-4 h-4" />
+                Tasks
               </h4>
-              <p className="text-sm text-muted-foreground leading-relaxed">
-                {anomaly.description}
-              </p>
-            </div> */}
 
-            <Separator />
-
-            {/* Status Update Form */}
-            {/* <StatusUpdateForm
-              currentStatus={anomaly.status}
-              onSubmit={(update) => onStatusUpdate(anomaly.id, update)}
-            /> */}
+              <div className="space-y-3">
+                {infrastructureDetail?.infrastructure_task.map(
+                  (item, index) => (
+                    <div
+                      key={item.id}
+                      className="flex gap-4 p-3 rounded-lg bg-muted/50"
+                    >
+                      <div className="flex flex-col items-center">
+                        {getStatusIcon(item.status)}
+                        {index <
+                          infrastructureDetail?.infrastructure_task.length -
+                            1 && <div className="w-px h-8 bg-border mt-2" />}
+                      </div>
+                      <div className="flex-1 min-w-0">
+                        <div className="flex items-center justify-between mb-3">
+                          {getStatusBadge(item.status)}
+                          <span className="text-xs text-muted-foreground">
+                            {formatDate(new Date(item.created_at))}
+                          </span>
+                        </div>
+                        <div>
+                          <p className="text-sm text-muted-foreground">
+                            Tasks : {item.tasks}
+                          </p>
+                        </div>
+                        {/* {item. && (
+                          <p className="text-sm text-muted-foreground">
+                            {action.notes}
+                          </p>
+                        )} */}
+                        {/* <p className="text-xs text-muted-foreground flex items-center gap-1 mt-1">
+                          <User className="w-3 h-3" />
+                          {action.operator}
+                        </p> */}
+                      </div>
+                    </div>
+                  )
+                )}
+              </div>
+            </div>
           </CardContent>
         </ScrollArea>
       </Card>
